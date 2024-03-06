314e06e26a441093e2bccba8e1dc8a7c742e7310
student-02-b1edbc847490 <kambletanaya18@gmail.com>
2024-03-05 20:34:34 -0800

    'sql'

---
+++ sql_cmd.sql
@@ -0,0 +1,11 @@
+show databases;create database tanaya;
+show databases;
+use tanaya;
+create table First(
+id int primary key not null,
+Name varchar(30),
+age int,
+city varchar(20)
+);
+show tables;
+desc First;
