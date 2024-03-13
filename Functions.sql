3381261a6a0db9ccaa83c1ca6a71b2106e8c90a0
student-02-b1edbc847490 <kambletanaya18@gmail.com>
2024-03-13 12:39:58 -0700

    'operator'

---
+++ Functions.sql
@@ -0,0 +1,11 @@
+	-- Function 
+use tanaya;
+select * from StudInfo;
+	-- aggregate function
+select max(Paid_fees) from StudInfo;
+select min(Paid_fees) from StudInfo;
+select sum(Paid_fees) from StudInfo;
+select avg(Paid_fees) from StudInfo;
+select count(Paid_fees) from StudInfo;
+
+	-- group by
--- Operators.sql
+++ Operators.sql
@@ -29,5 +29,31 @@ select distinct Paid_fees from StudInfo order by Paid_fees limit 1;
 
 	-- list
 select * from StudInfo where ID in (2,5,7);
+use tanaya;
+select * from StudInfo;
+select * from StudInfo where ID not in(5,9);
+delete from StudInfo where ID in (2,8);
 
 update StudInfo set Paid_fees = Paid_fees + ((Paid_fees*1.5)*100) where ID in (3,7);
+
+	-- between
+select * from StudInfo where ID between 3 and 6;
+select * from studinfo where Paid_fees between 20000 and 30000;
+select * from StudInfo where Paid_fees>=20000 and Paid_fees<=30000;
+
+	-- set operators
+create table table1(ID int , Name varchar(20));
+create table table2(ID int , Name varchar(20));
+insert into table1 values(1,'Tanaya'),(2,'Vihan'),(3,'Kasturi'),(4,'Isha'),(5,'Yuvaraj');
+insert into table2 values(1,'Khushi'),(2,'Vihan'),(3,'Tanaya'),(4,'Isha'),(5,'Anvi');
+select * from table1;
+select * from table2;
+	-- union  similar values consider single time
+select * from table1 union select * from table2;
+	-- union all combine all the values
+select * from table1 union all select * from table2;
+	-- intersect display similar values
+select * from table1 intersect select * from table2;
+	-- except display diffrent values from left table
+select * from table1 except select * from table2;
+select * from table2 except select * from table1;
